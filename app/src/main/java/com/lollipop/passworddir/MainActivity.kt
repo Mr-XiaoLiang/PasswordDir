33b95aacd3745f784200fa30144ace603896fd86
lollipop <liujian594@hotmail.com>
2021-10-21 21:56:50 +0800

    增加一些基础的东西

--- app/src/main/java/com/lollipop/passworddir/MainActivity.kt
+++ app/src/main/java/com/lollipop/passworddir/MainActivity.kt
@@ -2,10 +2,15 @@ package com.lollipop.passworddir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lollipop.passworddir.databinding.ActivityMainBinding
+import com.lollipop.passworddir.util.lazyBind
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazyBind()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
